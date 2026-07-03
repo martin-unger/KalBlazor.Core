e70bfa20e7ef1af149640e370fbd34bdce34e86b
Mario Trajkoski <mario.trajkoski.extern@orfnet.at>
2026-07-03 13:52:07 +0200

    feat: Add new SVG icons for arrows, info circle, file lines, graph, plus, minus, and triangle exclamation

--- src/KalBlazor.Core/Icons.cs
+++ src/KalBlazor.Core/Icons.cs
@@ -6,15 +6,25 @@ public static class Icons
     public const string AngleLeft = "angle-left";
     public const string AngleRight = "angle-right";
     public const string AngleUp = "angle-up";
+    public const string ArrowDown = "arrow-down";
+    public const string ArrowLeft = "arrow-left";
+    public const string ArrowRight = "arrow-right";
+    public const string ArrowUp = "arrow-up";
     public const string Bars = "bars";
+    public const string CircleInfo = "circle-info";
     public const string Clapperboard = "clapperboard";
     public const string Document = "document";
     public const string FileArrowUp = "file-arrow-up";
+    public const string FileLines = "file-lines";
     public const string Filter = "filter";
+    public const string Graph = "graph";
+    public const string Minus = "minus";
     public const string Pencil = "pencil";
+    public const string Plus = "plus";
     public const string Search = "search";
     public const string TableCells = "table-cells";
     public const string Trash = "trash";
+    public const string TriangleExclamation = "triangle-exclamation";
     public const string UserKey = "user-key";
     public const string X = "x";
 }
